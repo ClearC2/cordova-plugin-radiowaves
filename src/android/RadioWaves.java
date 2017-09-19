65fad7455b87257c0958d913d0a4986f58ef5a81
Jacob Allen <JacobRAllen1@gmail.com>
2017-09-19 13:06:27 -0500

    sending the string class to the application

--- src/android/RadioWaves.java
+++ src/android/RadioWaves.java
@@ -220,11 +220,12 @@ public class RadioWaves extends CordovaPlugin implements SignalStrengthListener 
 			if (cellInfoLte != null) {
 				CellSignalStrengthLte signalStrengthLte = cellInfoLte.getCellSignalStrength();
 				lteData.put("rssi", signalStrengthLte.getDbm());
-				if (Build.VERSION.SDK_INT >= 26) {
-					lteData.put("snr", signalStrengthLte.getRssnr());
-					lteData.put("rsrp", signalStrengthLte.getRsrp());
-					lteData.put("rsqr", signalStrengthLte.getRsrq());
-				}
+				lteData.put("string", signalStrengthLte.toString());
+				// if (Build.VERSION.SDK_INT >= 26) {
+				// 	lteData.put("snr", signalStrengthLte.getRssnr());
+				// 	lteData.put("rsrp", signalStrengthLte.getRsrp());
+				// 	lteData.put("rsqr", signalStrengthLte.getRsrq());
+				// }
 			}
 
 			// Signal
