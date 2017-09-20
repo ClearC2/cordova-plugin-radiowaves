19bd6e5f920b3f43837e392de260055d4aa5691b
Jacob Allen <JacobRAllen1@gmail.com>
2017-09-20 16:38:26 -0500

    added lte raw data string

--- src/android/RadioWaves.java
+++ src/android/RadioWaves.java
@@ -239,6 +239,7 @@ public class RadioWaves extends CordovaPlugin implements SignalStrengthListener 
 				lteData.put("dbm", signalStrengthLte.getDbm());
 				lteData.put("asu", signalStrengthLte.getAsuLevel());
 				String[] LTEData = signalStrengthLte.toString().split(" ");
+				lteData.put("raw", signalStrengthLte.toString())
 				for (int i = 0; i < LTEData.length; i++) {
 					String[] data = LTEData[i].split("=");
 					if (data.length == 2) {
